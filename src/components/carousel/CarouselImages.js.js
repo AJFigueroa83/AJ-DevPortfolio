2c567406ee688ea7b78e546bca14473c4e5a9286
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2023-01-07 00:04:27 -0500

    changed timing on image transition.

--- src/components/carousel/CarouselImages.js.js
+++ src/components/carousel/CarouselImages.js.js
@@ -14,7 +14,7 @@ class CarouselImages extends Component {
         return (
             <div className={classes.carousel_container} id="start">
                 {/* <ScrollAnimation offset="0" animateIn="fadeInLeft" duration='2.4' animateOnce="true" initiallyVisible ={true}> */}
-                <Carousel className={classes.carousel} dynamicHeight infiniteLoop={true} interval={6000} useKeyboardArrows={true} transitionTime={1700} emulateTouch showArrows={false} autoPlay showStatus={false} showThumbs={false} >
+                <Carousel className={classes.carousel} dynamicHeight infiniteLoop={true} interval={5000} useKeyboardArrows={true} transitionTime={1700} emulateTouch showArrows={false} autoPlay showStatus={false} showThumbs={false} >
                     <div className={classes.image_container} >
                         <img className={classes.image} src={Hello} alt="myImage" />
                         <div className={classes.h1}>
