abc2a672409b09924b7e8b5c451f74be789420c2
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2023-01-06 19:01:42 -0500

    updated the about me info.

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -11,8 +11,8 @@ class About extends Component {
                     <span className={classes.head}>ABOUT ME</span>
                     <h2 className={classes.heading}>Who Am I?</h2>
                     <div className={classes.About}>
-                        <p> My name is <b>SACHIN MITTAL</b> and I am currently working as Software Developer Engineer at <a target="_blank" href="https://www.springworks.in/"><b>Springworks</b></a>. I completed my degree in Bachelor of Technology in Computer Science and Engineering from Sant Longowal Institute of Engineering and Technology. I am much interested in developing new things which excite me a lot. :)  </p>
-                        <p className={classes.br}>I love exploring new technologies and being a practitioner, I like to stay on top of latest trends. I try to leave every line of code I write more readable, accessible, and modular.
+                        <p> My name is <b>ALFREDO J. FIGUEROA</b> and I have been in the hospitality industry for over 20 years. In today's everchanging world, a new path has emerged for me. I am now embarking on a career change into the world of web development. Currently I am a student in the Georgia Tech Coding bootcamp and I am looking forward to this exciting career.</p>
+                        <p className={classes.br}>I am a Full-stack web developer leveraging a hospitality management background to build a more intuitive user experience on the web. Recently earned a certificate in full stack development from Georgia Tech Coding Bootcamp, with newly developed skills in JavaScript, CSS, React.js, Node.js and responsive web design. With each project, the aim is to become better at developing more skills. Have worked on a team of three to develop a search/locator app that helps locals and travelers find destination breweries. Very excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web.
                         </p>
                     </div>
                 </ScrollAnimation>
--- src/components/carousel/CarouselImages.js.js
+++ src/components/carousel/CarouselImages.js.js
@@ -18,8 +18,8 @@ class CarouselImages extends Component {
                     <div className={classes.image_container} >
                         <img className={classes.image} src={Hello} alt="myImage" />
                         <div className={classes.h1}>
-                            <h1 >I'm Sachin</h1>
-                            <a href="https://drive.google.com/file/d/1XwQYRl06QtuNPT8v4fHkbajk1ll9LTbi/view?usp=sharing" rel="opener noreferrer" target="_blank">VIEW CV   <FaArrowCircleDown /></a>
+                            <h1 >I'm AJ</h1>
+                            <a href="https://docs.google.com/document/d/1qVJobsYyBXTOuK2lpQOJhI1Ta__ghH7_buDz3tF6WsQ/edit?usp=sharing" rel="opener noreferrer" target="_blank">VIEW CV   <FaArrowCircleDown /></a>
                         </div>
                     </div>
                     <div className={classes.image_container}>
@@ -27,7 +27,7 @@ class CarouselImages extends Component {
                         <div className={classes.h2}>
                             <h1> I love building</h1>
                             <h1>THINGS!!</h1>
-                            <a href="https://github.com/mittalsam98" rel="opener noreferrer" target="_blank">VIEW PROJECTS   <GoProject /></a>
+                            <a href="https://https://github.com/AJFigueroa83" rel="opener noreferrer" target="_blank">VIEW PROJECTS   <GoProject /></a>
                         </div>
                     </div>
                     {/* <div className={classes.image_container}>
